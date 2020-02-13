2f787a3e7c0d4150e77885a38fca1d4db50367bc
Dean <tibeloute@msn.com>
2020-02-13 15:21:30 +0100

    Début + header et footer
    Via un include, le header et le footer seront chargés automatiquements. Un élément reste à insérer, la valeur 'active' en fonction des pages du menu

---
+++ repeat/header_1.php
@@ -0,0 +1,28 @@
+<div class="container-fluid bg-dark">
+  <h1 class=" text-center text-warning">Tracteur en Weppes <small>Association</small></h1>
+</div>
+<div>
+  <ul class="nav bg-success justify-content-center">
+    <li class="nav-item">
+      <a class="nav-link text-warning text-uppercase" href="#">Accueil</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-warning text-uppercase" href="#">Programme</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-warning text-uppercase" href="#">L'association</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-warning text-uppercase" href="#">Galleries</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-warning text-uppercase" href="#">Média</a>
+    </li>
+  </ul>
+</div>
+
+
+<a class="navbar-brand text-warning" href="#">Tracteur en Weppes <small>Association</small></a>
+
+<img src="img/tew_logo.jpg" alt="logo" class="img-thumbnail float-left" style="width: 7%">
+
